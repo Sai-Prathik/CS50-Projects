677ce89192f2f85e720a361c881dcf9aef5d668c
Sai-Prathik <66540105+Sai-Prathik@users.noreply.github.com>
2021-02-14 03:35:09 +0530

    Q1,Q2,Q3,Q4

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>/",views.entry,name="get_entry"),
+    path("search/",views.search_item,name="search"),
+    path("new_entry/",views.new_entry,name="new_entry")
+     
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,reverse,HttpResponseRedirect
+from django.contrib import messages
 from . import util
 
 
@@ -8,3 +8,32 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request,title):
+     
+    info=util.get_entry(title)
+    if info:
+        return  render(request,"encyclopedia/entry.html",{"title":info,"t":title})
+    else:
+        return render(request,"encyclopedia/filedonotexist.html",{"title":title})
+
+def search_item(request):
+    value=request.GET.get("q")
+    l=[]
+    if not (util.get_entry(value)) is None:
+        return HttpResponseRedirect(reverse("get_entry",kwargs={"title":value}))
+    else:
+        for i in util.list_entries():
+            if value in i:
+                l.append(i)
+        return render(request,"encyclopedia/search-results.html",{"results":l})
+
+def new_entry(request):
+ if request.method=="POST":
+  title=request.POST.get("Title")
+  info=request.POST.get("info")
+  if title in util.list_entries():
+      messages.error(request,"Entry already exists")
+  else:
+      util.save_entry(title,info)
+      return HttpResponseRedirect(reverse("get_entry",kwargs={"title":title}))
+ return render(request,"encyclopedia/new_entry.html")
